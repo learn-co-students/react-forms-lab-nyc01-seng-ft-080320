94f11b2a433a61f998e0012fc578b0fdfc5b54d4
AmSmo <adam.smolenski@gmail.com>
2020-10-07 17:28:33 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,37 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  changeHandler = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+    
+  }
+  submitHandler = (e) => {
+    e.preventDefault()
+    if (!Object.values(this.state).some((ele) => ele === "")){
+      this.props.handleLogin(this.state)
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.submitHandler}>
+        
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.changeHandler}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.changeHandler}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,22 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      tweet: ""
+    };
+  }
+
+  changeHandler = (e) => {
+    console.log(e.target.value)
+    this.setState({tweet: e.target.value})
   }
 
   render() {
     return (
       <div>
+        <div>Characters left: {(this.props.maxChars - this.state.tweet.length)}</div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.changeHandler} value={this.state.tweet}/>
       </div>
     );
   }
