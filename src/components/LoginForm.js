fefd195f0ca3276bbb47036ca041857902859fc2
russellsutt <russell.sutter@gmail.com>
2020-10-07 15:42:51 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,50 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleUsernameChange = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handlePasswordChange = (event) => {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  submitHandler = (event) => {
+    event.preventDefault();
+    if (this.state.username && this.state.password) {
+      let formData = { username: this.state.username, password: this.state.password }
+      this.props.handleLogin(formData)
+    } else {
+      window.alert("Must fill in both username and password")
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.submitHandler(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={event => this.handleUsernameChange(event)} id="username" name="username" type="text" value={this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={event => this.handlePasswordChange(event)} id="password" name="password" type="password" value={this.state.password}/>
           </label>
         </div>
         <div>
