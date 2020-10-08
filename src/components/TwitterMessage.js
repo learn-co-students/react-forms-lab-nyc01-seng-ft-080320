fbbeb9866644e30b8f164462c3eb109a701ed63a
vincentcollis <vinsanity0050@gmail.com>
2020-10-07 20:30:07 -0400

    stable. deliverable 1 complete but must refactor to reverse count

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,37 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      charCount: 0,
+      message: "",
+    };
+  }
+
+  handleCharCount = (event) => {
+    let currentCount = event.target.value.length
+    let currentMessage = event.target.value
+    
+    currentCount+1 >= this.props.maxChars ? this.setState({}) : this.setState({
+      charCount:event.target.value.length,
+      message: currentMessage
+    })
+    console.log(this.state)
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input onChange={this.handleCharCount}
+          type="text" 
+          name="message" 
+          id="message"
+          value = {this.state.message} 
+        />
+        
       </div>
     );
   }
