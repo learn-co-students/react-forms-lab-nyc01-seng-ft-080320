7cf33b09f9092036c7990e4182ea2b0bc56971b4
slurio <samantha.lurio@gmail.com>
2020-10-07 21:13:26 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,42 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  inputHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+      e.preventDefault()
+
+      // if(e.target.name.value !== "" && e.target.password.value !== "") {
+      //   return this.props.handleLogin(this.state)}
+
+    if (!this.state.username || !this.state.password) return
+
+    this.props.handleLogin(this.state)
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" onChange={this.inputHandler} name="username" type="text" value={this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" onChange={this.inputHandler} name="password" type="password" value={this.state.password}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,24 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      text: ""
+    };
   }
 
+  renderCount = (e) => {
+    // let newCount = this.state.count -= 1
+    // console.log
+    this.setState({text: e.target.value})
+  } 
+
   render() {
+    let count = this.props.maxChars - this.state.text.length
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" onChange={this.renderCount} name="message" id="message" value={this.state.text}/>
+        <span>{count}</span>
       </div>
     );
   }
